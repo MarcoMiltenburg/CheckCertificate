90bedcfed488b3e6e5d4647660459568ee6e0d79
Marco Miltenburg <marco@virtualpages.nl>
2025-06-04 20:05:56 +0200

    Initial commit of completely rewritten code, updated to .NET 8.

---
+++ src/CertificateChecker.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CheckCertificate
+{
+    internal partial class CertificateChecker
+    {
+        /// <summary>
+        /// Regular expression to extract subject name from ASN data.
+        /// </summary>
+        /// <returns></returns>
+        [GeneratedRegex("DNS Name=(.+)", RegexOptions.IgnoreCase, "en-US")]
+        private static partial Regex DNSNameRegEx();
+
+        /// <summary>
+        /// Check the certificate on the <paramref name="hostName"/> with port <paramref name="portNumber"/>.
+        /// </summary>
+        /// <param name="hostName">The host name.</param>
+        /// <param name="portNumber">The port number.</param>
+        /// <param name="warningDays">The number of days to warn before the certificate expires.</param>
+        /// <param name="SkipChainValidation">Set to true to skip validation of the entire chain.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A <see cref="ReturnValue"/> that has the result of the check.</returns>
+        internal async Task<ReturnValue> Check(string hostName, int portNumber, int warningDays, bool SkipChainValidation, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // Get the certificate of the host
+                X509Certificate2? certificate = await GetHostCertificate(hostName, portNumber);
+                if (certificate == null)
+                    return ReturnValue.CertificateInvalid;
+
+                // Check if the hostname meatches the certificate
+                if (!HostnameMatchesCertificate(certificate, hostName))
+                {
+                    Console.Error.WriteLine($"Certificate for {hostName} on port {portNumber} does not match the host name!");
+                    return ReturnValue.CertificateInvalidSubjectName;
+                }
+
+                // The current date/time
+                DateTimeOffset today = DateTimeOffset.Now;
+
+                // Check if the certificate is already valid
+                if (today < certificate.NotBefore)
+                {
+                    Console.Error.WriteLine($"Certificate for {hostName} on port {portNumber}is not yet valid!");
+                    return ReturnValue.CertificateNotValidYet;
+                }
+
+                // Check if the certificate has expired
+                if (today > certificate.NotAfter)
+                {
+                    Console.Error.WriteLine($"Certificate for {hostName} on port {portNumber} has expired!");
+                    return ReturnValue.CertificateIsExpired;
+                }
+
+                X509Chain chain = new();
+                chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EndCertificateOnly;
+
+                // Is the certificate revoked?
+                if (!chain.Build(certificate))
+                {
+                    Console.Error.WriteLine($"Certificate for {hostName} on port {portNumber} has been revoked!");
+                    return ReturnValue.CertificateHasBeenRevoked;
+                }
+
+                // Check the entire certificate chain
+                if (!SkipChainValidation)
+                { 
+                    // Change chain policy to entire chain
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+
+                    // Reset the chain
+                    chain.Reset();
+
+                    // Is the entire chain valid?
+                    if (!chain.Build(certificate))
+                    {
+                        Console.Error.WriteLine($"Certificate chain for the certificate of {hostName} on port {portNumber} is not valid!");
+
+                        // No, then output chain status messages
+                        foreach(var chainStatus in chain.ChainStatus)
+                        {
+                            Console.Error.WriteLine(chainStatus.StatusInformation);
+                        }
+
+                        return ReturnValue.CertificateChainInvalid;
+                    }
+                }
+
+                // Check if the certificate is almost expired
+                DateTimeOffset warningDateTime = today.AddDays(warningDays);
+                if (warningDateTime > certificate.NotAfter)
+                {
+                    // Calculate the number of days left
+                    var timeLeft = certificate.NotAfter - today;
+                    var daysLeft = Math.Round(timeLeft.TotalDays, MidpointRounding.AwayFromZero);
+
+                    if (daysLeft < 2)
+                    {
+                        Console.Error.WriteLine($"Certificate for {hostName} on port {portNumber} expires within {timeLeft.Hours:D2}:{timeLeft.Minutes:D2} hrs!");
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine($"Certificate for {hostName} on port {portNumber} expires within {daysLeft} day(s)!");
+                    }
+
+                    return ReturnValue.CertificateAlmostExpired;
+                }
+
+                // Certificate is valid
+                Console.WriteLine("Certificate is valid.");
+                return ReturnValue.Success;
+            }
+            catch (OperationCanceledException)
+            {
+                Console.Error.WriteLine("Execution was aborted due to pressing Ctrl-C");
+                return ReturnValue.ExecutionWasAborted;
+            }
+        }
+
+        /// <summary>
+        /// Gets the certificate of the host.
+        /// </summary>
+        /// <param name="hostName">The host name</param>
+        /// <param name="portNumber">The port number</param>
+        /// <returns>Returns the certificate of the host if successfull, null in case of failure.</returns>
+        private async Task<X509Certificate2?> GetHostCertificate(string hostName, int portNumber)
+        {
+            TlsClient tlsClient = new();
+            if (!await tlsClient.ConnectToHost(hostName, portNumber))
+            {
+                Console.Error.WriteLine(tlsClient.HasError ? tlsClient.ErrorMessage : $"Unable to connect to {hostName} on port {portNumber}.");
+                return null;
+            }
+
+            return new X509Certificate2(tlsClient.ServerCertificate!);
+        }
+
+        /// <summary>
+        /// Check if the <paramref name="hostName"/> matches the subject name or one of the subject alternative names of the <paramref name="certificate"/>.
+        /// </summary>
+        /// <param name="certificate">The certificate.</param>
+        /// <param name="hostName">The host name</param>
+        /// <returns>True when the host name matches subject or a subject alternative name, false otherwise.</returns>
+        private bool HostnameMatchesCertificate(X509Certificate2 certificate, string hostName)
+        {
+            bool hostnameValid;
+            List<string>? subjectAlternativeNames = [];
+
+            // Get certificate subject name
+            var subjectName = certificate.GetNameInfo(X509NameType.SimpleName, false);
+
+            // We need at least a subject name
+            if (string.IsNullOrWhiteSpace(subjectName))
+                return false;
+
+            // Loop through all extensions to find the subject alternative names
+            foreach (X509Extension extension in certificate.Extensions)
+            {
+                // Is this an alternative name extension
+                if (extension.Oid?.Value != null && extension.Oid!.Value!.Equals(OidName.subjectAltName))
+                {
+                    // Create an AsnEncodedData object using the extension's information.
+                    AsnEncodedData asndata = new(extension.Oid, extension.RawData);
+
+                    // Parse the subject alternatice names
+                    subjectAlternativeNames = ParseSubjectAltNames(asndata);
+                }
+            }
+
+            // Check of the hostname matches the subject name
+            if (!(hostnameValid = HostnameMatch(hostName, subjectName)))
+            {
+                // It doesn't, do we have subject alternative names?
+                if (subjectAlternativeNames != null)
+                { 
+                    // So let's check the subject alternative names for a match
+                    foreach (string subjectAlternativeName in subjectAlternativeNames)
+                    {
+                        if (hostnameValid = HostnameMatch(hostName, subjectName))
+                            break;
+                    }
+                }
+            }
+
+            return hostnameValid;
+        }
+
+
+        /// <summary>
+        /// Parses the <paramref name="asnData"/> to a list of subject alternative names.
+        /// </summary>
+        /// <param name="asnData">The encoded ASN data.</param>
+        /// <returns>A list of subject alternative names if successfull. Null in case of a failure.</returns>
+        private List<string>? ParseSubjectAltNames(AsnEncodedData asnData)
+        {
+            if (asnData == null)
+                return null;
+
+            // Split on carriage return and newline
+            char[] asnSeparators = ['\r', '\n'];
+
+            // Parse raw data into an array of strings
+            string[] dnsNames = asnData.Format(true).Split(asnSeparators, StringSplitOptions.RemoveEmptyEntries);
+            if (!(dnsNames.Length > 0))
+                return null;
+
+            var result = new List<string>();
+
+            Regex regex = DNSNameRegEx();
+            foreach(string dnsName in dnsNames)
+            {
+                // Does it match the DNS name format?
+                Match match = regex.Match(dnsName);
+                if (match.Success && match.Groups.Count > 1)
+                {
+                    // Then take group "1" as that contains the match on the dns name
+                    var subjectAltName = match.Groups[1].Value;
+                    if (!string.IsNullOrEmpty(subjectAltName))
+                        result.Add(subjectAltName);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks if the <paramref name="hostName"/> matches the <paramref name="subjectName"/>.
+        /// </summary>
+        /// <param name="hostName">The host name</param>
+        /// <param name="subjectName">The subject name.</param>
+        /// <returns></returns>
+        private bool HostnameMatch(string hostName, string subjectName)
+        {
+            // Create a regular expression that conforms to the wild matching rules for subject names
+            string wildcardMatch = string.Format("^{0}$", subjectName.Replace(".", "\\.").Replace("*", "[^.]+"));
+            Regex regex = new(wildcardMatch, RegexOptions.IgnoreCase);
+
+            return regex.IsMatch(hostName);
+        }
+
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+
+namespace CheckCertificate
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The default port number to use.
+        /// </summary>
+        private const int PortNumberDefaultValue = 443;
+
+        /// <summary>
+        /// The minimum allowed value for the port number.
+        /// </summary>
+        private const int PortNumberMinimumValue = 1;
+
+        /// <summary>
+        /// The maximum allowed value for the port number.
+        /// </summary>
+        private const int PortNumberMaximumValue = 65535;
+
+        /// <summary>
+        /// The default number of days to warn before a certificate expires.
+        /// </summary>
+        private const int WarningDaysDefaultValue = 14;
+
+        /// <summary>
+        /// The minimum allowed value for warning days.
+        /// </summary>
+        private const int WarningDaysMinimumValue = 0;
+
+        /// <summary>
+        /// The maximum allowed value for warning days.
+        /// </summary>
+        private const int WarningDaysMaximumValue = 365;
+
+        /// <summary>
+        /// The default value of the skip chain validation.
+        /// </summary>
+        private const bool SkipChainValidationDefaultValue = false;
+
+        /// <summary>
+        /// Main entry point of the application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>A return value indicating the result of the execution.</returns>
+        static async Task<int> Main(string[] args)
+        {
+            ReturnValue returnValue = 0;
+
+            // Argument: Host name
+            var hostArgument = new Argument<string>
+            (
+                name: "host",
+                description: "The hostname to connect to."
+            );
+
+            // Option: Port number
+            var portOption = new Option<int>
+            (
+                name: "--port",
+                description: "Override the port number to use.",
+                getDefaultValue: () => PortNumberDefaultValue
+            );
+
+            // Validator for the port number
+            portOption.AddValidator(ValidateOptionMinimumAndMaximum(portOption, PortNumberMinimumValue, PortNumberMaximumValue));
+
+            //Option: Warning days
+            var warningDaysOption = new Option<int>
+            (
+                name: "--warningdays",
+                description: "Number of days to warn before certificate expires.",
+                getDefaultValue: () => WarningDaysDefaultValue
+            );
+
+            // Validator for warning days
+            portOption.AddValidator(ValidateOptionMinimumAndMaximum(warningDaysOption, WarningDaysMinimumValue, WarningDaysMaximumValue));
+
+            // Options: skip chain validation
+            var skipChainValidationOption = new Option<bool>
+            (
+                name: "--skip-chain-validation",
+                description: "Skip checking of the entire certificate chain.",
+                getDefaultValue: () => SkipChainValidationDefaultValue
+            );
+
+            // Create root command
+            var rootCommand = new RootCommand
+            {
+                hostArgument,
+                portOption,
+                warningDaysOption,
+                skipChainValidationOption
+            };
+
+            // Set handler and pass arguments and options
+            rootCommand.SetHandler(async (context) =>
+            {
+                string hostName = context.ParseResult.GetValueForArgument(hostArgument);
+                int portNumber = context.ParseResult.GetValueForOption(portOption);
+                int warningDays = context.ParseResult.GetValueForOption<int>(warningDaysOption);
+                bool skipChainValidation = context.ParseResult.GetValueForOption<bool>(skipChainValidationOption);
+
+                var checker = new CertificateChecker();
+                returnValue = await checker.Check(hostName, portNumber, warningDays, skipChainValidation, context.GetCancellationToken());
+            });
+
+            // Invoke handler
+            await rootCommand.InvokeAsync(args);
+
+            // Return the result
+            return (int)returnValue;
+        }
+
+        /// <summary>
+        /// Validate that the value of the <paramref name="option"/> is not lower than <paramref name="minimumValue"/> and not higher than <paramref name="maximumValue"/>.
+        /// </summary>
+        /// <param name="option">The option to check.</param>
+        /// <param name="minimumValue">The minimum value to check for.</param>
+        /// <param name="maximumValue">The maximum value to check for.</param>
+        /// <returns></returns>
+        private static ValidateSymbolResult<OptionResult> ValidateOptionMinimumAndMaximum(Option<int> option, int minimumValue, int maximumValue)
+        {
+            return result =>
+            {
+                // Get value for the option
+                int value = result.GetValueForOption(option);
+
+                // Make sure it's withing the boundaries, or else set error message
+                if (value < minimumValue || value > maximumValue)
+                {
+                    result.ErrorMessage = $"The value of option '{option.Name}' must be between {minimumValue} and {maximumValue}.";
+                }
+            };
+        }
+    }
+}
